4b0a6902e2c6f27139da102492f188c1bb1f6782
Rajasekhar <rajasekhartechpy@gmail.com>
2021-07-05 23:34:27 +0530

    initial commit

---
+++ __settings__.js
@@ -0,0 +1,22 @@
+ASSET_PREFIX = "";
+SCRIPT_PREFIX = "";
+SCENE_PATH = "1186451.json";
+CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': false,
+    'preserveDrawingBuffer': false,
+    'preferWebGl2': true,
+    'powerPreference': "default"
+};
+SCRIPTS = [ 50734748, 50739754, 50743715, 50767438, 50773028, 50878341, 50887305, 50944260, 50997811, 50997812, 51084320 ];
+CONFIG_FILENAME = "config.json";
+INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+PRELOAD_MODULES = [
+    {'moduleName' : 'Ammo', 'glueUrl' : 'files/assets/50987719/1/ammo.wasm.js', 'wasmUrl' : 'files/assets/50987720/1/ammo.wasm.wasm', 'fallbackUrl' : 'files/assets/50987718/1/ammo.js', 'preload' : true},
+];
